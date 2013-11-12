5d47fbebe87796647ad43ad97960c60c03f864fb
ylyan <ylyan@adobe.com>
2013-11-12 15:25:34 +0800

    chapter 2 ex2

---
+++ src/chapter2/ThreeArguments.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class ThreeArguments {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i=0; i < args.length; i++){
+			System.out.println(i);
+			System.out.println(args[i]);
+		}
+		System.out.println("ending...");
+	}
+
+}
