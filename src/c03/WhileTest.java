e8f8936f40c9281c86a558a65b084d6aaf18dde6
ylyan <ylyan@adobe.com>
2013-11-15 09:49:41 +0800

    c03 examples

---
+++ src/c03/WhileTest.java
@@ -0,0 +1,13 @@
+package c03;
+
+//: WhileTest.java
+//Demonstrates the while loop
+public class WhileTest {
+	public static void main(String[] args) {
+		double r = 0;
+		while (r < 0.99d) {
+			r = Math.random();
+			System.out.println(r);
+		}
+	}
+} // /:~
