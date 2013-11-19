b2a9f9ef384477032116af6b40f153fdc85a4495
ylyan <ylyan@adobe.com>
2013-11-19 09:49:09 +0800

    c04

---
+++ src/c04/DefaultConstructor.java
@@ -0,0 +1,12 @@
+package c04;
+
+//: DefaultConstructor.java
+class Bird {
+	int i;
+}
+
+public class DefaultConstructor {
+	public static void main(String[] args) {
+		Bird nc = new Bird(); // default!
+	}
+} // /:~
