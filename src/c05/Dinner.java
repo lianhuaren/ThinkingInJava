3dd85bbf0d7cd94da86f0f41ec86a82b65d28921
ylyan <ylyan@stepstone.corp.adobe.com>
2013-12-05 10:21:35 +0800

    c05

---
+++ src/c05/Dinner.java
@@ -0,0 +1,14 @@
+package c05;
+
+import c05.dessert.*;
+
+public class Dinner {
+	public Dinner() {
+		System.out.println("Dinner constructor");
+	}
+
+	public static void main(String[] args) {
+		Cookie x = new Cookie();
+		// ! x.foo(); // Can't access
+	}
+} // /:~
---
+++ src/c05/dessert/Cookie.java
@@ -0,0 +1,13 @@
+//: Cookie.java
+// Creates a library
+package c05.dessert;
+
+public class Cookie {
+	public Cookie() {
+		System.out.println("Cookie constructor");
+	}
+
+	void foo() {
+		System.out.println("foo");
+	}
+} // /:~
