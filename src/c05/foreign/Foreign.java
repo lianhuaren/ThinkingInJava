3c963ead3d0109a5b70937b7325a4a9dd9dd5e41
ylyan <ylyan@adobe.com>
2013-12-11 10:46:28 +0800

    c06

--- src/c05/foreign/Foreign.java
+++ src/c05/foreign/Foreign.java
@@ -5,6 +5,6 @@ import c05.*;
 
 public class Foreign {
 	public static void main(String[] args) {
-		PackagedClass pc = new PackagedClass();
+		//PackagedClass pc = new PackagedClass();
 	}
 } // /:~
