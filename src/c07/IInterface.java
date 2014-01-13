0f48a534d0a970c5f960d4b3291b41bc10760415
ylyan <ylyan@adobe.com>
2014-01-14 06:44:53 +0800

    c07 and c08

---
+++ src/c07/IInterface.java
@@ -0,0 +1,15 @@
+package c07;
+
+//: IInterface.java
+//Static inner classes inside interfaces
+class IInterface {
+	static class Inner {
+		int i, j, k;
+
+		public Inner() {
+		}
+
+		void f() {
+		}
+	}
+} // /:~
