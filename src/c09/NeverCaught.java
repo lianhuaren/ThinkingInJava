c59d2f781902695f2e268ad49fd9103bad5ca7ca
ylyan <ylyan@adobe.com>
2014-01-17 09:09:10 +0800

    c09

---
+++ src/c09/NeverCaught.java
@@ -0,0 +1,17 @@
+package c09;
+
+//: NeverCaught.java
+//Ignoring RuntimeExceptions
+public class NeverCaught {
+	static void f() {
+		throw new RuntimeException("From f()");
+	}
+
+	static void g() {
+		f();
+	}
+
+	public static void main(String[] args) {
+		g();
+	}
+} // /:~
