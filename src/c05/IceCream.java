bbbe402671cb28fb5d45a45db0499f26430ccc16
ylyan <ylyan@stepstone.corp.adobe.com>
2014-01-26 08:53:37 +0800

    c05 IceCream

---
+++ src/c05/IceCream.java
@@ -0,0 +1,20 @@
+package c05;
+
+//: IceCream.java
+//Demonstrates "private" keyword
+class Sundae {
+	private Sundae() {
+		System.out.println("private Sundae() is called.");
+	}
+
+	static Sundae makeASundae() {
+		return new Sundae();
+	}
+}
+
+public class IceCream {
+	public static void main(String[] args) {
+		// ! Sundae x = new Sundae();
+		Sundae x = Sundae.makeASundae();
+	}
+} // /:~
