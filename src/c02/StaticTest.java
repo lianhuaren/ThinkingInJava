0e1967d9e4da66e14aed7d77f199940a40727836
Yuelin37 <yuelinyan@gmail.com>
2014-09-04 14:29:22 +0800

    StaticTest

---
+++ src/c02/StaticTest.java
@@ -0,0 +1,12 @@
+package c02;
+
+public class StaticTest {
+	static int i = 47;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(StaticTest.i);
+
+	}
+
+}
